fcc67f85648f19f02c01f0ca67765b263a317af3
Parth Modi <parth.modi9510@gmail.com>
2020-06-16 12:36:50 +0530

    changed profile picture and logic changes in help page

--- src/Containers/help/help-section/index.tsx
+++ src/Containers/help/help-section/index.tsx
@@ -1,4 +1,4 @@
-import React, { Fragment, useState, useEffect, FC } from 'react';
+import React, { Fragment, useState, FC } from 'react';
 import { History } from 'history'
 
 type propType = {
@@ -16,11 +16,21 @@ const HelpSection: FC<propType>= ({ data, history, panelData }) => {
 	const [selectedPanel, changeSelectedPanel] = useState(Number(data[2]));
 
 	const changePanel = (panel: any, isLink: Boolean) => () => {
-		if (!isLink) changeSelectedPanel(panel)
+		if (selectedPanel === -1 && panel === Number(data[2])) {
+			changeSelectedPanel(panel)
+			return;
+		}
+		if (selectedPanel === panel) {
+			changeSelectedPanel(-1);
+			return;
+		}
+		if (!isLink) {
+			changeSelectedPanel(panel)
+		}
 		history.push(`/help/${panel}`);
 	}
 
-	const linkId = data[data.length - 1]
+	const linkId = data[3]
 
 	return (
 		<div className="help-section-wrapper__left-section-wrapper">
@@ -37,23 +47,26 @@ const HelpSection: FC<propType>= ({ data, history, panelData }) => {
 										</div>
 									</div>
 								</li>
-								<div className="children-wrapper">
-									{
-										selectedPanel === data.id ? data.children.map((c: childrenType) => {
-											return (
-												<li key={c.id}>
-													<div className="wrap">
-														<div className={`left-margin ${linkId === c.id ? 'selected' : ''}`} />
-														<div onClick={changePanel(`${data.id}/${c.id}`, true)}>
-															<div className="heading">
-																<span>{c.name}</span>
+								<div>
+									<div className="children-wrapper">
+										{
+											selectedPanel === data.id ? data.children.map((c: childrenType) => {
+												debugger
+												return (
+													<li key={c.id}>
+														<div className="wrap">
+															<div className={`left-margin ${linkId === c.id ? 'selected' : ''}`} />
+															<div onClick={changePanel(`${data.id}/${c.id}`, true)}>
+																<div className="heading">
+																	<span>{c.name}</span>
+																</div>
 															</div>
 														</div>
-													</div>
-												</li>
-											)
-										}) : null
-									}
+													</li>
+												)
+											}) : null
+										}
+									</div>
 								</div>
 							</Fragment>
 						)
