927e7372065a928fc4d9dc563d729f5923532c73
Parth Modi <parthmodi.developer@gmail.com>
2020-06-18 22:03:24 +0530

    made panelData method and used to get the data

--- src/Containers/help/display-section/help-pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import AirbnbFriendlyProgramme from '../../getting-started/deciding-to-host/airbnb-friendly-programme';
-import WhoCanHostOnAirbnb from '../../getting-started/deciding-to-host/who-can-host-on-airbnb';
-
-const Links = {
-	AirbnbFriendlyProgramme,
-	WhoCanHostOnAirbnb
-};
-
-export default Links;
